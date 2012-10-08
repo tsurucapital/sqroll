131127996374bf13f51e8757d1f295e35697a402
Jasper Van der Jeugt <m@jaspervdj.be>
2012-10-08 14:07:22 +0900

    Add a test on defaults

--- tests/Database/Sqroll/Tests.hs
+++ tests/Database/Sqroll/Tests.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Database.Sqroll.Tests
     ( tests
     ) where
 
 import Control.Applicative ((<$>))
+import Data.ByteString.Char8 ()
 import Data.IORef (modifyIORef, newIORef, readIORef)
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
@@ -11,11 +13,14 @@ import System.Directory (getTemporaryDirectory, removeFile)
 import System.FilePath ((</>))
 
 import Database.Sqroll
+import qualified Database.Sqroll.Tests.ModifiedTypes as ModifiedTypes
+import Database.Sqroll.Sqlite3
 import Database.Sqroll.Tests.Types
 
 tests :: Test
 tests = testGroup "Database.Sqroll.Tests"
     [ testCase "testAppendTailUsers" testAppendTailUsers
+    , testCase "testModifiedTypes"   testModifiedTypes
     ]
 
 testAppendTailUsers :: Assertion
@@ -29,6 +34,27 @@ testAppendTailUsers = withTmpScroll $ \sqroll -> do
 
     testUsers' <- reverse <$> readIORef ref
     testUsers @=? testUsers'
+
+testModifiedTypes :: Assertion
+testModifiedTypes = do
+    -- Insert a user type
+    (tmpPath, sqroll) <- sqrollOpenTmp
+    append            <- sqrollAppend sqroll Nothing
+    append $ User "John" "Doe" 32 "kittens"
+    rowid <- sqlLastInsertRowId (sqrollSql sqroll)
+    sqrollClose sqroll
+
+    -- Read a modified user type
+    sqroll' <- sqrollOpen tmpPath
+    select  <- sqrollSelect sqroll' (Just ModifiedTypes.defaultUser)
+    user    <- select rowid
+    sqrollClose sqroll'
+
+    -- Check that they are equal, defaults worked, everyone is happy, rainbows,
+    -- unicorns etc.
+    ModifiedTypes.User "John" "Doe" "m@jaspervdj.be" 32 @=? user
+
+    removeFile tmpPath
 
 withTmpScroll :: (Sqroll -> IO a) -> IO a
 withTmpScroll f = do
---
+++ tests/Database/Sqroll/Tests/ModifiedTypes.hs
@@ -0,0 +1,24 @@
+-- | This module carries types, that slightly differ from the ones in the types
+-- module, so we can test migrations between types
+{-# LANGUAGE DeriveGeneric #-}
+module Database.Sqroll.Tests.ModifiedTypes
+    ( User (..)
+    , defaultUser
+    ) where
+
+import Data.ByteString (ByteString)
+import GHC.Generics (Generic)
+
+import Database.Sqroll (HasTable)
+
+data User = User
+    { userFirstName :: String
+    , userLastName  :: String
+    , userEmail     :: String
+    , userAge       :: Int
+    } deriving (Eq, Generic, Show)
+
+instance HasTable User
+
+defaultUser :: User
+defaultUser = User "Jasper" "Van der Jeugt" "m@jaspervdj.be" 22
