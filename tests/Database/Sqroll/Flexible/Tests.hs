68fb7be065e6465c900c6c8e3689f21c70ccd7f5
Michael Baikov <manpacket@gmail.com>
2012-12-10 10:30:18 +0800

    move a bunch of test related code from library to tests

--- tests/Database/Sqroll/Flexible/Tests.hs
+++ tests/Database/Sqroll/Flexible/Tests.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Database.Sqroll.Flexible.Tests (
   tests
@@ -10,7 +11,8 @@ module Database.Sqroll.Flexible.Tests (
 
 import Control.Applicative
 import Data.ByteString.Char8 ()
-import Database.Sqroll
+import Database.Sqroll (HasTable, sqrollAppend_, sqrollGetList)
+import GHC.Generics (Generic)
 import Database.Sqroll.Flexible
 import Database.Sqroll.Tests.Util
 import Test.Framework (Test, testGroup)
@@ -45,3 +47,98 @@ testFlexible = withTmpSqroll $ \sqroll -> do
                    , RRRR 100 (Just 110.0) True
                    ]
     expected @=? result
+
+
+-- createStmtX should just typecheck
+
+createStmt :: IO ()
+createStmt = do
+    _ <- constructQuery undefined $ do
+        t `LeftJoin` r <- from
+        where_ $ (r ^?. Bar) >. just 100
+        where_ $ var 100 >. (t ^. Bar)
+        on_ ((t ^. Foo) ==? (r ^?. Foo))
+        order_ $ asc (t ^. Foo)
+        return $ RRRR <$> (t ^. Foo) <*> (r ^?. Bar +. just 10) <*> (var True) -- +. t ^. Bar *. var 10) <*. (var True)
+    return ()
+
+createStmt2 :: IO ()
+createStmt2 = do
+    _ <- constructQuery undefined $ do
+        t1 `InnerJoin` t2 `InnerJoin` t3 <- from
+        on_ ((t1 ^. Foo) ==. (t2 ^. Foo))
+        on_ ((t2 ^. Foo) ==. (t3 ^. Foo))
+        return $ Intx3 <$> (t1 ^. Foo) <*> (t2 ^. Foo) <*> (t3 ^. Foo)
+    return ()
+
+
+createStmt3 :: IO ()
+createStmt3 = do
+    _ <- constructQuery undefined $ do
+        (t1 :: Exp HaskTag TTTT) <- from
+        return $ t1
+    return ()
+
+
+createStmt4 :: IO ()
+createStmt4 = do
+    _ <- constructQuery undefined $ do
+        t1 `InnerJoin` t2 `InnerJoin` t3 `InnerJoin` t4 `InnerJoin` t5 `InnerJoin` t6 <- from
+        on_ ((t5 ^. Foo) ==. (t6 ^. Foo))
+        on_ ((t4 ^. Foo) ==. (t5 ^. Foo))
+        on_ ((t3 ^. Foo) ==. (t4 ^. Foo))
+        on_ ((t2 ^. Foo) ==. (t3 ^. Foo))
+        on_ ((t1 ^. Foo) ==. (t2 ^. Foo))
+        return $ Intx6 <$>  (t1 ^. Foo) <*> (t2 ^. Foo) <*> (t3 ^. Foo) <*> (t4 ^. Foo) <*> (t5 ^. Foo) <*> (t6 ^. Foo)
+    return ()
+
+createStmt5 :: IO ()
+createStmt5 = do
+    _ <- constructQuery undefined $ do
+        t1 `LeftJoin` t2 `LeftJoin` t3 `InnerJoin` t4 <- from
+        return $ (,,,) <$> (t1 ^. Foo) <*> (t2 ^?. Foo) <*> (t3 ^?. Foo) <*> t4 ^?. Foo
+    return ()
+
+data Intx3 = Intx3 Int Int Int deriving (Generic)
+instance HasTable Intx3
+
+data Intx6 = Intx6 Int Int Int Int Int Int deriving (Generic)
+instance HasTable Intx6
+
+_unused_ok :: IO ()
+_unused_ok = do
+    _ <- undefined $ (createStmt, createStmt2, createStmt3, createStmt4, createStmt5)
+    _ <- undefined $ (tFoo, tBar, foo, bar, baz)
+    _ <- undefined $ Baz
+
+    return ()
+
+
+-- sample table in the db
+data TTTT = TTTT { tFoo :: Int, tBar :: Double } deriving (Show, Generic)
+
+instance HasTable TTTT
+
+-- sample result datatype
+data RRRR = RRRR { foo :: Int, bar :: Maybe Double, baz :: Bool } deriving (Eq, Show)
+
+data Foo = Foo
+data Bar = Bar
+data Baz = Baz
+
+type instance Component TTTT Foo = Int
+type instance Component TTTT Bar = Double
+type instance Component TTTT Baz = Bool
+
+
+instance IsTag Foo where
+    type Full = TTTT
+    getTagName _ = "t_foo"
+
+instance IsTag Bar where
+    type Full = TTTT
+    getTagName _ = "t_bar"
+
+instance IsTag Baz where
+    type Full = TTTT
+    getTagName _ = "t_baz"
