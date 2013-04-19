29498741ffe0ac0b7ef334dd3f08712a9f2f2497
Niklas Hambüchen <mail@nh2.me>
2013-04-19 10:37:27 +0800

    Use sqlite3_close_v2() instead of sqlite3_close().
    Fixes segfault on GC after sqrollClose (e.g. on program termination).

--- cbits/sqroll.c
+++ cbits/sqroll.c
@@ -1,5 +1,6 @@
 #include "sqroll.h"
 
+/* Finalizes a statement. Used as GC finalizer in `sqlPrepare` in Sqlite3.hs. */
 int sqroll_finalize_stmt(sqlite3 *db, sqlite3_stmt *stmt) {
     sqlite3_stmt *i = sqlite3_next_stmt(db, 0);
     int status = 0;
@@ -16,13 +17,19 @@ int sqroll_finalize_stmt(sqlite3 *db, sqlite3_stmt *stmt) {
     return status;
 }
 
+
 int sqroll_close(sqlite3 *db) {
-    sqlite3_stmt *i = 0;
 
-    do {
-        sqlite3_finalize(i);
-        i = sqlite3_next_stmt(db, 0);
-    } while (i != 0);
+    /* Use the v2 version of sqlite3_close here which is designed to be used
+       with garbage-collected host languages.
+       It will make the database inaccessible, but not free its resources
+       until the last statement associated with it has been finalized;
+       once that happens, the database will close itself.
 
-    return sqlite3_close(db);
+       This is necessary because garbage collection can call sqlite3_finalize
+       on GC-managed statements *after* the user has manually closed
+       the database - this is exactly what happens in sqroll_finalize_stmt above,
+       which is registered as a statement finalizer in `sqlPrepare` in Sqlite3.hs.
+    */
+    return sqlite3_close_v2(db);
 }
